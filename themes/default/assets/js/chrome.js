07f74ffa1e57d59382b451f0a85f28e5beebc59a
bluebabes <cantfinduser@abc.com>
2021-03-21 00:15:35 +0800

    init

---
+++ themes/default/assets/js/chrome.js
@@ -0,0 +1,24 @@
+(function () {
+    var installButtons = document.getElementsByClassName('js-install-button')
+
+    for (var i = 0; i < installButtons.length; i++) {
+        var installButton = installButtons[i]
+
+        if (chrome.app.isInstalled) {
+            installButton.innerHTML = 'INSTALLED, THANKS!'
+            installButton.className += ' installed-button'
+
+        } else {
+            installButton.addEventListener('click', function (event) {
+                var href = this.href
+                event.preventDefault()
+
+                chrome.webstore.install(href, function () { }, function (data) {
+                    console.log('Whops, inline install failed, continue with default link')
+                    console.log(data)
+                    window.location = href
+                })
+            }, false)
+        }
+    }
+})()
